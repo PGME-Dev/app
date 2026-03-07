00bec974e708ebfd430690669b631be58ebd3dc2
Yatharth Kaushal <yatharthkaushal@proton.me>
2026-03-07 15:57:06 +0530

    join pgme

--- lib/features_ios/settings/screens/careers_screen.dart
+++ lib/features_ios/settings/screens/careers_screen.dart
@@ -35,7 +35,7 @@ class _CareersScreenState extends State<CareersScreen> {
 
   static const List<String> _roles = [
     'Lecturer',
-    'Medical Editor',
+    'Examiner',
     'Subject Coordinator',
     'Marketing & Growth',
     'Operations & Support',
